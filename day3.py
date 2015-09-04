913a6cde5a13c21c25fde9d1e6c943411899b919
ryomajia <shindousaijia@hotmail.com>
2015-09-04 15:07:15 +0800

    day3.py

--- day3.py
+++ day3.py
@@ -1,2 +1,36 @@
 __author__ = 'juliajia'
 
+#!/usr/bin/env python
+
+class queue:
+
+    errormessage = 'error'
+    def __init__(self, queue_name, num = 0):
+        self.queue_name = []
+        self.n = num
+    def pop(self):
+        if self.queue_name == []:
+            return self.errormessage
+        else:
+            a = self.queue_name.pop()
+            self.n = self.n - 1
+        return a
+
+    def push(self, queue_element):
+        self.queue_name.insert(0,queue_element)
+        self.n = self.n + 1
+
+
+    def front(self):
+        front_element = self.queue_name[0]
+        return front_element
+
+
+    def back(self):
+        back_element = self.queue_name[self.n - 1]
+        return back_element
+
+
+    def size(self):
+        count = self.n
+        return count
---
+++ queue_test.py
@@ -0,0 +1,20 @@
+__author__ = 'juliajia'
+
+
+def test_queue():
+    from day3 import queue
+    a = queue("test")
+    a.pop()
+    assert (a.errormessage=="error")
+    a.push(4)
+    assert (a.size()==1)
+    assert (a.front()==4)
+    assert (a.back()==4)
+    a.push(2)
+    assert (a.size()==2)
+    assert (a.front()==2)
+    assert (a.back()==4)
+    a.pop()
+    assert (a.size()==1)
+    assert (a.front()==2)
+    assert (a.back()==2)
